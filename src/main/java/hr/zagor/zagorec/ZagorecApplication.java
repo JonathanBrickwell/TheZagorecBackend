7434a06518c8998caa771056600a893d9d71e6b8
Kristijan Fištrek <kristijan.fistrek@t.ht.hr>
2020-11-24 08:28:45 +0100

    Master: Initial Commit

---
+++ src/main/java/hr/zagor/zagorec/ZagorecApplication.java
@@ -0,0 +1,13 @@
+package hr.zagor.zagorec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZagorecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZagorecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/zagor/zagorec/ZagorecApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.zagor.zagorec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZagorecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
